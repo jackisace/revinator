e1e29f12a0d80f1ca423e723455db2948f32b705
Jack <jack@gmail.com>
2024-10-12 18:05:46 +0100

    u

--- connect.py
+++
@@ -1,8 +0,0 @@
-#!/bin/python
-
-import os
-
-os.system("clear")
-cwd = os.getcwd()
-cwd = cwd.split("/")[-1]
-os.system(f"(echo {cwd}; cat) | nc 127.0.0.1 4444") 
--- connect.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-clear
-
-ip=$(pwd | rev | cut -d "/" -f 1 | rev)
-
-/bin/bash -s -i -l -c "(printf $ip ; cat) | nc 127.0.0.1 4444" --
